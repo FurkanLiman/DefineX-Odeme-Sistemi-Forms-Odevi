7d21668d3aaf4b599a6b90d8e52466d7f18911dd
Furkan Liman <limanfurkan@outlook.com>
2025-03-06 13:12:41 +0300

    Creating Windows Forms Project

---
+++ DefineX-Odeme-Sistemi-Forms-Odevi/DefineX-Odeme-Sistemi-Forms-Odevi/Form1.cs
@@ -0,0 +1,10 @@
+namespace DefineX_Odeme_Sistemi_Forms_Odevi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
